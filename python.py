67370f7eca3f9d9b5800e6e4483f258f0f3036c3
Valpaq <viktor.paramonov2000@gmail.com>
2022-06-14 21:07:56 +0300

    buble

--- python.py
+++ python.py
@@ -2,26 +2,30 @@ import json
 tmp = open('whitelist.json')
 df = json.load(tmp)
 print(len(df))
-# for i in range(len(df)):
-#     tmp_mints = df[i]['whitelisted_mints']
-#     del df[i]['whitelisted_mints']
-#     df[i]['is_trusted_volume'] = False
-#     if "banksea_info_post_url" in df[i].keys():
-#         pass
-#     else:
-#         df[i]['banksea_info_post_url'] = None
-#     if "hyperspaceSlug" in df[i].keys():
-#         pass
-#     else:
-#         df[i]['hyperspaceSlug'] = None
+for i in range(len(df)):
+    tmp_mints = df[i]['whitelisted_mints']
+    del df[i]['whitelisted_mints']
+    df[i]['is_trusted_volume'] = False
+    if "banksea_info_post_url" in df[i].keys():
+        pass
+    else:
+        df[i]['banksea_info_post_url'] = None
+    if "hyperspaceSlug" in df[i].keys():
+        pass
+    else:
+        df[i]['hyperspaceSlug'] = None
 
-#     if "max_amount_of_active_loans" in df[i].keys():
-#         pass
-#     else:
-#         df[i]['max_amount_of_active_loans'] = 15
-#     df[i]['whitelisted_mints'] = tmp_mints
-    # if df[i]['creator'] == "9BKWqDHfHZh9j39xakYVMdr6hXmCLHH5VfCpeq2idU9L":
-    #     print(i)
+    if "tensor_slug" in df[i].keys():
+        pass
+    else:
+        df[i]['tensor_slug'] = None
+
+    if "max_amount_of_active_loans" in df[i].keys():
+        pass
+    else:
+        df[i]['max_amount_of_active_loans'] = 15
+    df[i]['whitelisted_mints'] = tmp_mints
+
 
 with open("whitelist.json", "w") as file:
     json.dump(df, file, indent = 1)
