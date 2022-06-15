c47ccad969bcfd2e7e5aa91d8916700fb2fc16f3
Valpaq <viktor.paramonov2000@gmail.com>
2022-06-15 16:51:05 +0300

    the remnants

--- python.py
+++ python.py
@@ -1,15 +1,15 @@
 import json
-tmp = open('testing_whitelist.json')
+tmp = open('whitelist.json')
 df = json.load(tmp)
 print(len(df))
-for i in range(len(df)):
-    tmp_mints = df[i]['whitelisted_mints']
-    del df[i]['whitelisted_mints']
-    df[i]['time_based_liquidity_pool'] = df[i]['liquidity_pool']
-    df[i]['price_based_liquidity_pool'] = df[i]['liquidity_pool']
-    del df[i]['liquidity_pool']
-    df[i]['whitelisted_mints'] = tmp_mints
+# for i in range(len(df)):
+#     tmp_mints = df[i]['whitelisted_mints']
+#     del df[i]['whitelisted_mints']
+#     df[i]['time_based_liquidity_pool'] = df[i]['liquidity_pool']
+#     df[i]['price_based_liquidity_pool'] = df[i]['liquidity_pool']
+#     del df[i]['liquidity_pool']
+#     df[i]['whitelisted_mints'] = tmp_mints
 
-with open("testing_whitelist.json", "w") as file:
+with open("whitelist.json", "w") as file:
     json.dump(df, file, indent = 1)
     
