32dc458394143a2bc19c23e7569e11acbea95b5b
Valpaq <viktor.paramonov2000@gmail.com>
2022-09-20 20:16:18 +0300

    updated volume

--- add_tiers.py
+++ add_tiers.py
@@ -7,10 +7,13 @@ tiers = pd.read_csv("tiers.csv")
 for i in wl:
     tmp_mints = i['whitelisted_mints']
     del i['whitelisted_mints']
-    try:
-        i['tier'] = int(tiers.loc[tiers['name'] == i['name'], 'tier'].values[0])
-    except:
-        i['tier'] = 3
+    if 'tier' in i.keys():
+        pass
+    else:
+        try:
+            i['tier'] = int(tiers.loc[tiers['name'] == i['name'], 'tier'].values[0])
+        except:
+            i['tier'] = 3
     i['volume7d'] = 0
     i['volume_with_tiers'] = 0
     i['whitelisted_mints'] = tmp_mints
